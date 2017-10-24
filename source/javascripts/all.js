b17d0ad38c93c359e0fb0211cd40b34dad384694
Josef Zacek <info@josefzacek.com>
2017-10-24 21:59:06 +0100

    hide .home-page-video if user is on tablet and mobile devices

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,7 @@
 //= require popper
 //= require bootstrap
+
+// hide home-page-video if user is on tablet and mobile devices (otherwise video not working on tablet/mobile)
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  $('.home-page-video').css('visibility', 'hidden');
+}
