c1a90f7b76f175f871f6958d13cf272e2d2c8966
Josef Zacek <info@josefzacek.com>
2017-11-19 16:40:01 +0000

    js for contact page map

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,4 +6,17 @@ $( document ).ready(function() {
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $('.home-page-video').css('visibility', 'hidden');
   }
+
+  // leaflet map on contact page
+  var map = L.map('leaflet-map').setView([53.349729, -6.260631], 11);
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([53.349729, -6.260631]).addTo(map).bindPopup('JAWA Ireland');
+
+  map.dragging.disable();
+  map.scrollWheelZoom.disable();
+
 });
