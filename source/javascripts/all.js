02455f1d362e47412ac4fc5c167765f1929ff672
Josef Zacek <info@josefzacek.com>
2017-11-19 16:39:47 +0000

    move to $( document ).ready(function() { });

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,9 +1,9 @@
 //= require popper
 //= require bootstrap
 
-// hide home-page-video if user is on tablet and mobile devices (otherwise video not working on tablet/mobile)
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $('.home-page-video').css('visibility', 'hidden');
-}
 $( document ).ready(function() {
+  // hide home-page-video if user is on tablet and mobile devices (otherwise video not working on tablet/mobile)
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('.home-page-video').css('visibility', 'hidden');
+  }
 });
