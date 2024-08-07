c56d8f7fa057c8e596523626b78366a0c5b969f8
Josef Zacek <joezacek@gmail.com>
2024-08-07 12:49:46 +0100

    remove all code related to old masonry loading on vintage posters page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -59,37 +59,7 @@ $( document ).ready(function() {
     });
   });
 
-  // init Masonry on vintage posters
-  var $grid = $('.masonry-grid').masonry({
-    itemSelector: 'none', // select none at first
-    columnWidth: '.grid__col-sizer',
-    gutter: '.grid__gutter-sizer',
-    percentPosition: true,
-    stagger: 30,
-    // nicer reveal transition
-    visibleStyle: { transform: 'translateY(0)', opacity: 1 },
-    hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
-  });
 
-  // get Masonry instance
-  var msnry = $grid.data('masonry');
-
-  // initial items reveal
-  $grid.imagesLoaded( function() {
-    $grid.removeClass('are-images-unloaded');
-    $grid.masonry( 'option', { itemSelector: '.grid__item' });
-    var $items = $grid.find('.grid__item');
-    $grid.masonry( 'appended', $items );
-  });
-
-  // init Infinte Scroll on vintage posters
-  $grid.infiniteScroll({
-    path: '.pagination__next',
-    append: '.grid__item',
-    outlayer: msnry,
-    status: '.page-load-status',
-    hideNav: '.pagination',
-  });
 
   // pop up on models page
   $(document).on('click', '[data-toggle="lightbox"]', function (event) {
